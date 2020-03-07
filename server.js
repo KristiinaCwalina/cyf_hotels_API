f6215b0d55adf220931a76d5aabdbdd84172fdee
User0017 <user0017@Administrators-MacBook-Pro-3.local>
2020-03-07 13:57:35 +0100

    adding API route, password

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+const secrets = require('./secrets');
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'cyf_hotels',
+    password: secrets.dbPassword,
+    port: 5432
+});
+
+app.get("/hotels", function(req, res) {
+    pool.query('SELECT * FROM hotels', (error, result) => {
+        res.json(result.rows);
+    });
 });
